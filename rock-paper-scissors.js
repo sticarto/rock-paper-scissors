b3e0a433772460e954688e661d1676c65d3aeb88
sticarto <123905512+sticarto@users.noreply.github.com>
2024-07-16 16:32:17 -0700

    Add function that plays the game for 5 rounds

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,8 +1,5 @@
 
 
-let humanScore;
-let computerScore;
-
 function getComputerChoice() {
     // Get a random number to choose which action the computer will take.
     let computerChoice = Math.floor(Math.random() * 3);
@@ -22,39 +19,52 @@ function getHumanChoice() {
 
     // check if the choice is valid
 
-
     // return that string
     return humanChoice;
 }
 
-function playRound(humanChoice, computerChoice) {
-    // compare both string arguments to see who wins
-    if (humanChoice.toUpperCase() === "ROCK" && computerChoice === "SCISSORS") {
-        console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
-        humanScore += 1;
-    } else if (humanChoice.toUpperCase() === "PAPER" && computerChoice === "ROCK") {
-        console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
-        humanScore += 1;
-    } else if (humanChoice.toUpperCase() === "SCISSORS" && computerChoice === "PAPER") {
-        console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
-        humanScore += 1;
-    } else if (humanChoice.toUpperCase() === computerChoice) {
-        console.log(`Tie! ${humanChoice} vs ${computerChoice}`);
-    } else {
-        console.log("You lost");
-        computerScore += 1;
+
+function playGame() {
+    // Move score variables here
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // Move playRound function here. This lets it access the score variables.
+    function playRound(humanChoice, computerChoice) {
+        // compare both string arguments to see who wins
+        if (humanChoice.toUpperCase() === "ROCK" && computerChoice === "SCISSORS") {
+            console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
+            humanScore += 1;
+        } else if (humanChoice.toUpperCase() === "PAPER" && computerChoice === "ROCK") {
+            console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
+            humanScore += 1;
+        } else if (humanChoice.toUpperCase() === "SCISSORS" && computerChoice === "PAPER") {
+            console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
+            humanScore += 1;
+        } else if (humanChoice.toUpperCase() === computerChoice) {
+            console.log(`Tie! ${humanChoice} vs ${computerChoice}`);
+        } else {
+            console.log("You lost");
+            computerScore += 1;
+        }
     }
 
-    // display text stating who won
+    // Call playRound 5 times
+    for (let i = 0; i < 5; i++) {
+        // get choices
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
 
-    // increment the winner's score
+        // run playround
+        playRound(humanSelection, computerSelection);
+
+    }
+
+    // Display the score
+    console.log(`The Final Score: HUMAN = ${humanScore} | COMPUTER = ${computerScore}`);
+
 }
 
 
 
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
-
-
-playRound(humanSelection, computerSelection);
-
+playGame();
