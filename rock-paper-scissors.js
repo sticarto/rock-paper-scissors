96c5eb44c5028b9f80642b655a1d61dad12ba053
sticarto <123905512+sticarto@users.noreply.github.com>
2024-07-16 16:05:03 -0700

    Add the logic for a single round

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -24,15 +24,37 @@ function getHumanChoice() {
 
 
     // return that string
-    return humanChoice.toUpperCase();
+    return humanChoice;
 }
 
 function playRound(humanChoice, computerChoice) {
+    // compare both string arguments to see who wins
+    if (humanChoice.toUpperCase() === "ROCK" && computerChoice === "SCISSORS") {
+        console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
+        humanScore += 1;
+    } else if (humanChoice.toUpperCase() === "PAPER" && computerChoice === "ROCK") {
+        console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
+        humanScore += 1;
+    } else if (humanChoice.toUpperCase() === "SCISSORS" && computerChoice === "PAPER") {
+        console.log(`You Won! ${humanChoice} beats ${computerChoice}.`);
+        humanScore += 1;
+    } else if (humanChoice.toUpperCase() === computerChoice) {
+        console.log(`Tie! ${humanChoice} vs ${computerChoice}`);
+    } else {
+        console.log("You lost");
+        computerScore += 1;
+    }
 
+    // display text stating who won
+
+    // increment the winner's score
 }
 
 
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
+
+
+playRound(humanSelection, computerSelection);
 
